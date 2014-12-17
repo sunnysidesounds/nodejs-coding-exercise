094bec5b0319743cfc7ec4554a044bc4ebf47579
Jason R Alexander <sunnysidesounds@gmail.com>
2014-12-17 09:54:56 -0800

    Basic error handling and clean-up

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-/*
- * Module dependencies
- */
+// Module dependencies
 var express = require('express')
     , stylus = require('stylus')
     , nib = require('nib')
-    , JsonDB = require('node-json-db')
-
-
-var rawJson = require("./database.json")
+    , rawJson = require("./database.json")
 
 var app = express()
 function compile(str, path) {
@@ -19,31 +14,36 @@ function compile(str, path) {
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 app.use(express.logger('dev'))
-app.use(stylus.middleware(
-    { src: __dirname + '/public'
-        , compile: compile
-    }
-))
+app.use(stylus.middleware({ src: __dirname + '/public', compile: compile}))
 app.use(express.static(__dirname + '/public'))
+
 
 // home
 app.get('/', function (req, res) {
-    res.render('index', {
-        data: rawJson
-    })
+    if(rawJson.length != 0)
+        res.render('index', {
+            data: rawJson
+        })
+    else
+        throw new Error("No values in database");
 })
 
-// get all puppies
-app.get('/get/all', function (req, res) {
-    res.json(rawJson)
-})
 
 // get puppy by name
 app.get('/get/:name', function (req, res) {
-    res.render('detail', {
-        name: req.param("name"),
-        data: rawJson
-    })
+    if(rawJson.length != 0)
+        for (var i=0; i<rawJson.length; i++){
+            if(req.param("name") == rawJson[i].name)
+                res.render('detail', {
+                    name: req.param("name"),
+                    data: rawJson[i]
+                })
+            else
+                //TODO: Instead of throwing a error, we should display a message in the UI
+                throw new Error(req.param("name") + " doesn't exist in database");
+        }
+    else
+        throw new Error("No values in database");
 })
 
 app.listen(3000);
